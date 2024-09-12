2bb557bac02dba3fbedaccdd39a6c8970ee5bfe4
Álisson <alissonwegner@gmail.com>
2024-09-11 23:33:35 -0300

    up

--- suppliers/views.py
+++ suppliers/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from . import models
 # Create your views here.
+
+
+class SuppliersListView(ListView):
+    model = models.Supplier
+    template_name = 'suppliers_list.html'
+    context_object_name = 'suppliers'
