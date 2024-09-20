d668faffe5c741750536c9c596df248a683e3671
Álisson <alissonwegner@gmail.com>
2024-09-20 12:14:03 -0300

    aula 31

--- categories/urls.py
+++ categories/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('caregories/list/', views.CaregoryListView.as_view(), name='caregories_list'),
-    path('caregories/create/', views.CaregoryCreateView.as_view(), name='caregories_create'),
+    path('categories/list/', views.CategoryListView.as_view(), name='category_list'),
+    path('categories/create/', views.CategoryCreateView.as_view(), name='category_create'),
     #path('brands/create/', views.BrandCreateView.as_view(), name='brand_create'),
-    path('caregories/<int:pk>/detail/', views.CaregoryDetailView.as_view(), name='caregories_detail'),
-    path('caregories/<int:pk>/update/', views.CaregoryUpdateView.as_view(), name='caregories_update'),
-    path('caregories/<int:pk>/delete/', views.CaregoryDeleteView.as_view(), name='caregories_delete'),
+    path('categories/<int:pk>/detail/', views.CategoryDetailView.as_view(), name='category_detail'),
+    path('categories/<int:pk>/update/', views.CategoryUpdateView.as_view(), name='category_update'),
+    path('categories/<int:pk>/delete/', views.CategoryDeleteView.as_view(), name='category_delete'),
 
     #path('api/v1/brands/', views.BrandCreateListAPIView.as_view(), name='brand-create-list-api-view'),
     #path('api/v1/brands/<int:pk>/', views.BrandRetrieveUpdateDestroyAPIView.as_view(), name='brand-detail-api-view'),
--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+#from rest_framework import generics
+#from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from . import models, forms
+#, forms, serializers
 
-# Create your views here.
+
+#class BrandListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+class CategoryListView(ListView):
+    model = models.Category
+    template_name = 'category_list.html'
+    context_object_name = 'categories'
+    paginate_by = 5
+    #permission_required = 'brands.view_brand'
+
+    #colocar filtro no site
+    def get_queryset(self):
+     queryset = super().get_queryset()
+     name = self.request.GET.get('name')
+     if name:
+         queryset = queryset.filter(name__icontains=name)
+     return queryset
+
+
+#class BrandCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+class CategoryCreateView(CreateView):
+
+    model = models.Category
+    template_name = 'category_create.html'
+    form_class = forms.CategoryForm
+    success_url = reverse_lazy('category_list')
+#    permission_required = 'brands.add_brand'
+
+
+# class CaregoryDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
+class CategoryDetailView(DetailView):
+     model = models.Category
+     template_name = 'category_detail.html'
+     
+#     permission_required = 'brands.view_brand'
+
+
+# class BrandUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+class CategoryUpdateView(UpdateView):
+     model = models.Category
+     template_name = 'category_update.html'
+     form_class = forms.CategoryForm
+     success_url = reverse_lazy('category_list')
+#     permission_required = 'brands.change_brand'
+
+
+# class BrandDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+class CategoryDeleteView(DeleteView):
+     model = models.Category
+     template_name = 'caregory_delete.html'
+     success_url = reverse_lazy('category_list')
+#     permission_required = 'brands.delete_brand'
+
+
+# class BrandCreateListAPIView(generics.ListCreateAPIView):
+#     queryset = models.Brand.objects.all()
+#     serializer_class = serializers.BrandSerializer
+
+# class BrandRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = models.Brand.objects.all()
+#     serializer_class = serializers.BrandSerializer
