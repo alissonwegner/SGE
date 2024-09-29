8b5c13289d2561850c1db482c5c22ad09207aada
Álisson <alissonwegner@gmail.com>
2024-09-29 01:28:50 -0300

    Aula 42

--- app/views.py
+++ app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 def home(request):
-    context = {}
+    product_metrics = {
+        'total_cost_price': 10000,
+        'total_selling_price': 30000,
+        'total_quantity': 1000,
+        'total_profit': 200000
+    }
+    context = {
+        'product_metrics': product_metrics
+    }
     return render(request,'home.html', context)
