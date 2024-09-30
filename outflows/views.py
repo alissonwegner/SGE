3375e89f01b25384a117882d2df7de3ab12d248e
Álisson <alissonwegner@gmail.com>
2024-09-29 23:43:58 -0300

    Aula 47

--- outflows/views.py
+++ outflows/views.py
@@ -2,6 +2,7 @@
 #from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from app import metrics
 from . import models, forms
 #, forms, serializers
 
@@ -21,6 +22,10 @@ class OutflowListView(ListView):
             queryset = queryset.filter(product__title__icontains=product)
 
         return queryset
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['sales_metrics'] = metrics.get_sales_metrics()
+        return context
 
 #class BrandCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
 class OutflowCreateView(CreateView):
